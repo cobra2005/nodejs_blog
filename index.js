42be6c59c5c5923f7be3cf27ae8655f002d37a0c
unknown <you@example.com>
2025-02-06 16:59:59 +0700

    install morgan

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(morgan('tiny'));
+
 app.get('/home', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
